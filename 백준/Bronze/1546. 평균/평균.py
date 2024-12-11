21afd9e751131e4f093faae3df9b19a906843aa8
0122heart <145850192+0122heart@users.noreply.github.com>
2024-12-11 23:24:10 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,6 @@
-subjects = int(input())
-
+the_number_of_subjects = int(input())
 scores = list(map(int, input().split()))
-
-print(sum(scores) / max(scores) * 100 / subjects)
+highest_score = max(scores)
+new_scores = [score / highest_score * 100 for score in scores]
+new_average = sum(new_scores) / the_number_of_subjects
+print(new_average)
