bf758120f7586a6881dd2907221f25257e4c7b80
0122heart <145850192+0122heart@users.noreply.github.com>
2025-03-04 13:08:14 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,33 @@
+// 15649
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int arr[9] = {};
+int is_used[9] = {};
+
+void n_m(int now){
+    if(now == M){
+        for(int i = 0; i < M; i++) cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(!is_used[i]){
+            arr[now] = i;
+            is_used[i] = 1;
+            n_m(now + 1);
+            is_used[i] = 0;
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> M;
+    n_m(0);
+    return 0;
+}
